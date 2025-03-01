7ca74ad19c9aab92f6e8e621e4b33b145a000f7a
Shahanaj <fashionfuel267@gmail.com>
2025-03-01 16:58:42 +0600

    Add project files.

---
+++ B07ASPC11_Ecommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace B07ASPC11_Ecommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
